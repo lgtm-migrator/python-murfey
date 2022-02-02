b1a170898f44687cd7cd3d5c12bf71b5fe394ec8
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-02-02 15:56:29 +0000

    Add a Processor to keep a progress bar updated as new files are transferred

---
+++ src/murfey/utils/progress.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from rich.progress import Progress
+
+from murfey.utils import Processor
+from murfey.utils.rsync import RsyncPipe
+
+
+class ProgressMonitor(Processor):
+    def __init__(self, name: str = "progress_monitor"):
+        super().__init__(name=name)
+        self._progress = Progress()
+
+    def _process(self, **kwargs):
+        if isinstance(self._previous, RsyncPipe) and self._previous.thread:
+            while self._previous.thread.is_alive():
+                next_file = self._in.get()
+                current_total = len(
+                    [
+                        f
+                        for f in self._previous._previous.dir.glob("**/*")
+                        if not f.is_dir()
+                    ]
+                )
+                self._progress.update(total=current_total, advance=1)
+                self._out.put(next_file)
+
+    def wait(self):
+        super().wait()
+        self._progress.stop()
